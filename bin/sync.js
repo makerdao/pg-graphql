4c880dcdc35d96f416547983f98d847214022d86
dc <dc@log-ix.com>
2018-03-01 02:51:55 +0000

    names

--- bin/sync.js
+++ bin/sync.js
@@ -4,7 +4,6 @@ const args = process.argv.slice(2);
 const lib = require('../lib/common');
 const blockRunner = require('../libexec/sync-Blocks');
 const cupRunner = require('../libexec/sync-Cups');
-const newCupRunner = require('../libexec/sync-NewCups');
 const abi = cupRunner.abi
 const tub = cupRunner.tub;
 const gen = 4753930;
@@ -28,9 +27,12 @@ lib.web3.eth.getBlockNumber()
       sync(earliest, latest);
       if(data.length > diff)
         syncMissing(data);
-    });
-  });
-});
+    })
+    .catch(e => console.log("Web3 Error",e));
+  })
+  .catch(e => console.log("PG Error",e));
+})
+.catch(e => console.log("PG Error",e));
 
 const syncMissing = (data) => {
   let blocks = [];
@@ -84,9 +86,9 @@ tub.events.LogNote({
     console.log(e)
 })
 .on("data", (event) => {
-  cupRunner.syncCup(event);
+  cupRunner.writeCup(event);
 })
-.on("error", console.error);
+.on("error", console.log);
 
 // --------------------------------------------------------
 // Subscribe - New Cups
@@ -97,6 +99,6 @@ tub.events.LogNewCup({}, (e,r) => {
     console.log(e)
 })
 .on("data", (event) => {
-  newCupRunner.insertCup(event);
+  newCupRunner.writeNewCup(event);
 })
-.on("error", console.error);
+.on("error", console.log);
