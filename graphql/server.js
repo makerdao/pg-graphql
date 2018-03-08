201ce5e8627afc201c79a21e234b66d6e5830e9b
dc <dc@log-ix.com>
2018-03-08 22:43:41 +0000

    basic api rate-limiting

--- graphql/server.js
+++ graphql/server.js
@@ -1,24 +1,35 @@
 require('dotenv').config()
 
-const express = require('express');
+const express          = require('express');
 const { postgraphile } = require('postgraphile');
-const FilterPlugin = require('postgraphile-plugin-connection-filter');
+const FilterPlugin     = require('postgraphile-plugin-connection-filter');
+const RateLimit        = require('express-rate-limit');
 
 const app = express();
 
+// Rendering options for the index page
 app.engine('html', require('ejs').renderFile);
 app.set('views', 'graphql/views');
 
+// Display a page at the subdomain root
 app.get('/', (req, res) => res.render('index.html'))
 
-const options = {
+// Configure api routes
+const graphqlConfig = {
   graphiql: true,
   graphqlRoute: '/v1',
   graphiqlRoute: '/v1/console',
   appendPlugins: [FilterPlugin]
 }
 
-app.use(postgraphile(process.env.DATABASE_URL, 'public', options))
+const limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 30,             // 30 requests per IP
+  delayAfter: 10,      // slow responses after 10 requests
+  delayMs: 100         // by 1 second
+});
+
+app.use(postgraphile(process.env.DATABASE_URL, 'public', graphqlConfig))
 
 app.listen(process.env.PORT);
 console.log(`Running a GraphQL API server at localhost:${process.env.PORT}`)
