544fe1149916cfe7dc0d4e37d1ef5c6aeb7ccde2
dc <dc@log-ix.com>
2018-04-30 22:05:39 +0000

    order actions by idx

--- sql/migrate/003-functions.sql
+++ sql/migrate/003-functions.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION cup_actions(cup cup) RETURNS setof cup_act as $$
   SELECT *
   FROM cup_act
   WHERE cup_act.id = cup.id
-  ORDER BY cup_act.block DESC
+  ORDER BY cup_act.block DESC, cup_act.idx DESC
 $$ LANGUAGE SQL stable;
 
 CREATE OR REPLACE FUNCTION get_cup(id integer) RETURNS cup as $$
